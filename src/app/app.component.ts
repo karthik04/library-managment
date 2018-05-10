95b6669f26af6aeeeeadc8618fe7a8904e09d5b6
i310343 <karthik.p03@sap.com>
2018-05-10 12:31:06 +0530

    UI Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Library managment system';
+  title = 'Library Managment System';
 }
